cb19f1be764574661dacd3abc4912b0bb6be8b26
yehonatan2020 <yehonatan@yandex.ru>
2024-08-25 18:41:04 +0100

    common.mk: Add our own build.props etc

--- config/common.mk
+++ config/common.mk
@@ -64,6 +64,61 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.storage_manager.enabled=false
 
+# Use 2-button navbar by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.twobutton
+
+# Enable blur
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+dalvik.vm.checkjini=false \
+ro.kernel.android.checkjni=0 \
+ro.kernel.checkjni=0 \
+ro.config.nocheckin=1 \
+debug.systemui.latency_tracking=0 \
+persist.sample.eyetracking.log=0 \
+ro.com.google.locationfeatures=0 \
+ro.com.google.networklocation=0 \
+media.metrics.enabled=0 \
+sys.debug.watchdog=0 \
+logd.statistics=0 \
+media.metrics=0 \
+config.stats=0 \
+persist.sys.loglevel=0 \
+sys.log.app=0 \
+persist.traced.enable=0 \
+logd.statistics=0 \
+persist.sample.eyetracking.log=0 \
+debug.atrace.tags.enableflags=0 \
+debugtool.anrhistory=0 \
+ro.debuggable=1 \
+profiler.debugmonitor=false \
+profiler.launch=false \
+profiler.hung.dumpdobugreport=false \
+trustkernel.log.state=disable \
+debug.mdpcomp.logs=0 \
+debug.atrace.tags.enableflags=0 \
+pm.sleep_mode=1 \
+profiler.force_disable_ulog=true \
+profiler.force_disable_err_rpt=true \
+ro.logd.size.stats=0 \
+debug.atrace.tags.enableflags=0 \
+persist.service.pcsync.enable=0 \
+persist.service.lgospd.enable=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=0 \
+    persist.camera.HAL3.enabled=1 \
+    persist.camera.ois.disable=0
+
+$(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product, vendor/pixel-style/config/gms_full.mk)
+$(call inherit-product, vendor/pixel-style/config/common.mk)
+$(call inherit-product, vendor/certification/config.mk)
+
 # Inherit from audio config
 $(call inherit-product, vendor/pixel-style/config/audio.mk)
 
